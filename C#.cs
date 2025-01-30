6c5fcb8798b5ed91cac7a42f603cdafa581ef795
RANGO-top-1 <artur.detistov@bk.ru>
2025-01-30 13:59:07 +0300

    Add files via upload

---
+++ C#.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RANGO3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double d = (x*Math.Pow(y, 2)-Math.Sqrt(Math.Pow(x, 2) - 2.5*Math.Pow(10, -3)*y)/2*Math.Sin(x)*y)+0.5;
+            Console.WriteLine(" Ответ "+d);
+            Console.ReadLine();
+            
+        }
+    }
+}
